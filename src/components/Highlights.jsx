1493d6603fff62ada210ef72210c4190c96bf2b3
div40 <vidh40@gmail.com>
2024-03-18 19:38:48 +0530

    video showcase and highlights section

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,7 +1,48 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import React from "react";
+import { rightImg, watchImg } from "../utils";
+import VideoShowcase from "./VideoShowcase";
 
 const Highlights = () => {
-  return <div>Highlights</div>;
+  useGSAP(() => {
+    gsap.to("#title", {
+      opacity: 1,
+      y: 0,
+    });
+    gsap.to(".link", {
+      opacity: 1,
+      y: 0,
+      duration: 1,
+      stagger: 0.25,
+    });
+  }, []);
+  return (
+    <section
+      id="highlights"
+      className="w-screen h-full overflow-hidden common-padding bg-zinc"
+    >
+      <div className="screen-max-width">
+        <div className="flex items-end justify-between w-full mb-12">
+          <h1 id="title" className="section-heading font-sfdisplay">
+            Get the highlights.
+          </h1>
+          <div className="flex flex-wrap items-end gap-5">
+            <p className="link">
+              Watch the film
+              <img src={watchImg} alt="watch-film" className="ml-2" />
+            </p>
+            <p className="link">
+              Watch the event
+              <img src={rightImg} alt="watch-event" className="ml-2" />
+            </p>
+          </div>
+        </div>
+
+        <VideoShowcase />
+      </div>
+    </section>
+  );
 };
 
 export default Highlights;
