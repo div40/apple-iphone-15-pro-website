a00ef926d0370af0f13c9a30a0883ea7a9fbd361
div40 <vidh40@gmail.com>
2024-03-18 20:42:25 +0530

    phone model section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import Navbar from "./components/Navbar";
+import PhoneModel from "./components/PhoneModel";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Highlights />
+      <PhoneModel />
     </main>
   );
 };
