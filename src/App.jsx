e954e77a8c7426980a70b6f2a8c48f2c9f84b697
div40 <vidh40@gmail.com>
2024-03-25 22:22:35 +0530

    how it works section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Features from "./components/Features";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
+import HowItWorks from "./components/HowItWorks";
 import Navbar from "./components/Navbar";
 import PhoneModel from "./components/PhoneModel";
 
@@ -12,6 +13,7 @@ const App = () => {
       <Highlights />
       <PhoneModel />
       <Features />
+      <HowItWorks />
     </main>
   );
 };
---
+++ src/components/HowItWorks.jsx
@@ -0,0 +1,108 @@
+import React, { useRef } from "react";
+import { chipImg, frameImg, frameVideo } from "../utils";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { animateWithGsap } from "../utils/animations";
+
+const HowItWorks = () => {
+  const videoRef = useRef();
+
+  useGSAP(() => {
+    gsap.from("#chip", {
+      scrollTrigger: {
+        trigger: "#chip",
+        start: "20% bottom",
+      },
+      opacity: 0,
+      scale: 2,
+      duration: 2,
+      ease: "power2.inOut",
+    });
+
+    animateWithGsap(".g_fadeIn", {
+      opacity: 1,
+      y: 0,
+      duration: 1,
+      ease: "power2.inOut",
+    });
+  }, []);
+  return (
+    <section className="common-padding">
+      <div className="screen-max-width">
+        <div id="chip" className="w-full my-20 flex-center">
+          <img src={chipImg} alt="A17 pro chip" width={180} height={180} />
+        </div>
+
+        <div className="flex flex-col items-center">
+          <h2 className="tracking-wide hiw-title font-sfdisplay">
+            A17 Pro chip.
+            <br /> A monster win for gaming.
+          </h2>
+          <p className="tracking-wide hiw-subtitle font-sfdisplay">
+            It’s here. The biggest redesign in the history of Apple GPUs.
+          </p>
+        </div>
+
+        <div className="mt-10 md:mt-20 mb-14">
+          <div className="relative h-full flex-center">
+            <div className="overflow-hidden">
+              <img
+                src={frameImg}
+                alt="frame"
+                className="relative z-10 bg-transparent"
+              />
+            </div>
+            <div className="hiw-video">
+              <video
+                className="pointer-events-none"
+                playsInline
+                muted
+                preload="none"
+                autoPlay
+                ref={videoRef}
+              >
+                <source src={frameVideo} type="video/mp4" />
+              </video>
+            </div>
+          </div>
+          <p className="mt-3 font-semibold text-center text-gray font-sftext">
+            Honkai: Star Rail
+          </p>
+        </div>
+        <div className="hiw-text-container">
+          <div className="flex flex-col justify-center flex-1">
+            <p className="hiw-text g_fadeIn font-sftext">
+              A17 Pro is an entirely new class of iPhone chip that delivers our{" "}
+              {"  "}
+              <span className="text-white">
+                best graphics performance by far.
+              </span>{" "}
+            </p>
+
+            <p className="mt-6 hiw-text g_fadeIn font-sftext">
+              Mobile{" "}
+              <span className="text-white">
+                games will look and feel so immersive,
+              </span>{" "}
+              with incredibly detailed environments and more realistic
+              characters. And with industry-leading speed and efficiency, A17
+              Pro takes fast and runs with it.
+            </p>
+          </div>
+
+          <div className="flex flex-col justify-center flex-1 g_fadeIn">
+            <p className="hiw-text font-sfdisplay">New</p>
+            <p className="tracking-wide hiw-bigtext font-sfdisplay">
+              Pro-class GPU
+            </p>
+            <p className="tracking-wide hiw-text font-sfdisplay">
+              with 6 cores
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HowItWorks;
